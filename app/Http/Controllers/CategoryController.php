4b819bfd52ae288ee54906c0a2fa10be2b78be86
ekafebriana <ekafebriana171802@gmail.com>
2019-09-21 23:04:01 +0800

    tugas3

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categori = Category::all();
+        return view('category',compact('categori'));
+    }
+
+    
+    {
+        //
+    }
+}
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,6 +33,7 @@
 
                             </div>
                         </div>
+                        
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
@@ -44,6 +45,23 @@
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="captcha" class="col-md-4 col-form-label text-md-right">{{ __('captcha') }}</label>
+
+                            <div class="col-md-6">
+                                <div class="captcha">
+                                    <span>{!! captcha_img() !!}</span>
+                                    <button type="button" class="btn btn-success btn-refersh">Refersh</button>
+                            </div>
+                            <input type="text" id="captcha" class="form-control" placeholder="Enter Captcha" name="captcha">
+                            @if ($errors->has('captcha'))
+                                <span class="help-block">
+                                    <strong>{{$errors->first('captcha') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                         </div>
 
